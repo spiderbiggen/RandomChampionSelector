764ade712c251b7f303ad2353227f09f1f6ea755
Stefan Breetveld <spiderbiggen@gmail.com>
2026-07-31 10:37:08 +0200

    refactor: clean up plugin management and dependencyResolutionManagement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,13 @@
 pluginManagement {
     includeBuild("build-logic")
     repositories {
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
     }
@@ -13,6 +19,7 @@ plugins {
 }
 
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
